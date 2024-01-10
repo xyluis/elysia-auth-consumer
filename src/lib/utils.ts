4e5e9add240b7d0344c2e598c329f4d6b92ed9a3
Luís Felipe <luisfelipe.assuncao@hotmail.com>
2024-01-09 23:01:19 -0400

    omg help

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,11 +7,11 @@ export function cn(...inputs: ClassValue[]) {
 
 export function formatDate(input: string | number): string {
   const date = new Date(input)
-  
-  return date.toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
+
+  return date.toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
   })
 }
 
